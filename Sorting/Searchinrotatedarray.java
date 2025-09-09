849b62e2204320b34a61721652e2d41f72b7b19b
Pooja Bagodi <poojabagodi23@gmail.com>
2025-09-09 22:00:34 +0530

    Sorting

---
+++ Sorting/Searchinrotatedarray.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+public class Searchinrotatedarray {
+   public static int search(int arr[],int target,int si,int ei){
+    if(si>ei) return -1;
+    int mid=(si+ei)/2;
+    if(arr[mid]==target) return mid;
+    if(arr[si]<=arr[mid]){
+        if(target>=arr[si] && target<=arr[mid]){
+            return search(arr,target,si,mid-1);
+        }
+        else{
+            return search(arr,target,mid+1,ei);
+        }
+    }
+    else{
+        if(target>=arr[mid] && target<=arr[ei]){
+            return search(arr,target,mid+1,ei);
+        }
+        else{
+            return search(arr,target,si,mid-1);
+        }
+    }
+   }
+
+
+   public static void main(String[] args) {
+    int arr[]={4,5,6,7,0,1,2};
+    int target=0;
+    int ans=search(arr,target,0,arr.length-1);
+    System.out.println(ans);
+   }
+}
